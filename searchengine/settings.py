fae828f3a01ae090176d7ee79402bbae5e2ca8be
josefdaly <josefdaly@gmail.com>
2024-05-17 17:46:29 -0700

    try whitelist

--- searchengine/settings.py
+++ searchengine/settings.py
@@ -117,11 +117,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = [
-    'https://josefdaly.github.io/',
-]
-CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_WHITELIST = (
+    'https://josefdaly.github.io',
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
