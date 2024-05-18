7317f03cd2585fd3b5401a6991abbf3a1a2d07ed
josefdaly <josefdaly@gmail.com>
2024-05-17 17:23:34 -0700

    try this

--- searchengine/settings.py
+++ searchengine/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-wf9=i(-mus!nc42sa)-t4)si+txmnu=-mij&_j)2=!-%=!vu@g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'https://josefdaly.github.io/',
+]
 ALLOWED_HOSTS = settings_local.ALLOWED_HOSTS
 
 
